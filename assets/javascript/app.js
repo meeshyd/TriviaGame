f3b09afdc1d05e36b90e8de7d2a3f3cc015cdd5a
meeshyd <michellemdidier@gmail.com>
2017-03-10 21:41:12 -0600

    commiting changes to style.css, index.html, and game.js. Added remaining functions to game.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,28 @@
-
-// Game timer	Function for count down timer, ends game once time runs out		-5
-// Load question	Load question utilize “this”, and for loop to append to page		-5
-// Next question	Change from current questions to next question ++		-5
-// Done function	Detect if question answered is correct or incorrect		-5
-// Reset	Resets everything back to beginning or 0		-5
-// Results	Display the results to the player, incorrectly and correctly		-5
-// Include CDN	5 points will be deducted for not including the jQuery CDN properly		-5
-
-
 var trivia = [
 	{
-		question: "Which ingredients make up a negroni?",
+		question: "Which set of ingredients make a Negroni?",
 		possibleAnswers: [
 		"vodka, gin, bitters, orange peel",
-		"campari, gin, dry vermouth, lemon peel",
-		"campari, vodka, sweet vermouth, lemon peel",
-		"campari, gin, sweet vermouth, orange peel"
+		"Campari, gin, dry vermouth, lemon peel",
+		"Campari, vodka, sweet vermouth, lemon peel",
+		"Campari, gin, sweet vermouth, orange peel"
 		],
 		correctAnswer: 3,
-		image:""
+		image:"http://pipsum.com/200x310.jpg"
 	},
 	{
-		question: "Which ingredients make up a sazerac?",
+		question: "Which set of ingredients make a Sazerac?",
 		possibleAnswers: [
-		"whiskey, absinthe, orange juice, Peychaud’s bitters, orange peel",
-		"cognac or rye whiskey, absinthe, Peychaud’s bitters, sugar cube, lemon peel",
+		"whiskey, absinthe, orange juice, Peychaud's bitters, orange peel",
+		"cognac or rye whiskey, absinthe, Peychaud's bitters, sugar cube, lemon peel",
 		"cognac or rye whiskey, absinthe, lemon juice, lemon peel",
-		"whiskey, Peychaud’s bitters, sugar cube, lemon peel"
+		"whiskey, Peychaud's bitters, sugar cube, lemon peel"
 		],
 		correctAnswer: 1,
-		image:""
+		image:"http://pipsum.com/200x310.jpg"
 	},
 	{
-		question: "Which ingredients make up a manhattan?",
+		question: "Which set of ingredients make a Manhattan?",
 		possibleAnswers: [
 		"dry vermouth, whiskey, bitters, cherry",
 		"sweet vermouth, whiskey, bitters, cherry",
@@ -40,10 +30,10 @@ var trivia = [
 		"bourbon, dry vermouth, sugar cube, orange slice, cherry"
 		],
 		correctAnswer: 1,
-		image:""
+		image:"http://pipsum.com/200x310.jpg"
 	},
 	{
-		question: "Which ingredients make up a martini?",
+		question: "Which set of ingredients make a Martini?",
 		possibleAnswers: [
 		"white rum, dry vermouth, olive or lemon peel",
 		"white rum, sweet vermouth, olive or lemon peel",
@@ -51,10 +41,10 @@ var trivia = [
 		"gin, St. Germain, olive or lemon peel"
 		],
 		correctAnswer: 2,
-		image:""
+		image:"http://pipsum.com/200x310.jpg"
 	},
 	{
-		question: "Which ingredients make up a vesper?",
+		question: "Which set of ingredients make a Vesper?",
 		possibleAnswers: [
 		"vodka, gin, Lillet, lemon peel",
 		"gin, St. Germain, soda water, lemon peel",
@@ -62,10 +52,10 @@ var trivia = [
 		"vodka, gin, lemon-lime soda, lemon peel"
 		],
 		correctAnswer: 0,
-		image:""
+		image:"http://pipsum.com/200x310.jpg"
 	},
 	{
-		question: "Which ingredients make up a mojito?",
+		question: "Which set of ingredients make a Mojito?",
 		possibleAnswers: [
 		"vodka, mint, water, lemon juice",
 		"gin, mint, soda water, lime juice, sugar",
@@ -73,10 +63,10 @@ var trivia = [
 		"white rum, mint, soda water, lime juice, sugar"
 		],
 		correctAnswer: 3,
-		image:""
+		image:"http://pipsum.com/200x310.jpg"
 	},
 	{
-		question: "Which ingredients make up a white russian?",
+		question: "Which set of ingredients make a White Russian?",
 		possibleAnswers: [
 		"chocolate liquor, white rum, cream",
 		"coffee liquor, white rum, cream",
@@ -84,10 +74,10 @@ var trivia = [
 		"coffee liquor, vodka, egg white"
 		],
 		correctAnswer: 2,
-		image:""
+		image:"http://pipsum.com/200x310.jpg"
 	},
 	{
-		question: "Which ingredients make up a whiskey sour?",
+		question: "Which set of ingredients make a Whiskey Sour?",
 		possibleAnswers: [
 		"whiskey, orange, simple syrup, egg white, orange peel",
 		"whiskey, lemon juice, simple syrup, egg white, lemon peel",
@@ -95,82 +85,125 @@ var trivia = [
 		"whiskey, dry vermouth, lemon juice, egg white, lemon peel"
 		],
 		correctAnswer: 1,
-		image:""
+		image:"http://pipsum.com/200x310.jpg"
 	}
 ];
-
-var $triviaContainer = $("#trivia-container");
-var $timerDiv = $("#timer-div");
+var $triviaContainer = $('#trivia-container');
+var $timerDiv = $('#timer-div');
 var $questionDiv = $('#questions-div');
-var $answerBtnsDiv = $("#answer-buttons");
-var time = 30;
-var intervalId;
-var userChoice;
+var $answerChoicesDiv = $('#answer-choices');
+var $startResetDiv= $('#start-reset-div');
+var $endMessageDiv = $('#end-message');
+var $correctCtDiv = $('#correct-ct');
+var $wrongCtDiv = $('#wrong-ct');
+var $unansweredCtDiv = $('#unanswered-ct');
+var seconds; var intervalId; var userChoice; var currentQuestion;
+var correctAnswerCt; var wrongAnswerCt; var unansweredCt;
 
+$( document ).ready(function() {
+		currentQuestion = 0;
+		correctAnswerCt = 0;
+		wrongAnswerCt = 0;
+		unansweredCt = 0;
+		var $startButton = $('<button>');
+		$startButton.text("Start")
+		$startResetDiv.append($startButton);
 
-function start(){
-	// $('#finalMessage').empty();
-	// $('#correctAnswers').empty();
-	// $('#incorrectAnswers').empty();
-	// $('#unanswered').empty();
-	currentQuestion = 0;
-	correctAnswer = 0;
-	wrongAnswer = 0;
-	unanswered = 0;
-	renderQuestion();
-}
-
-start();
+		$startButton.on('click', function() {
+			renderQuestion();
+		});
+});
 
 function renderQuestion(){
-
+	$answerChoicesDiv.empty();
+	$startResetDiv.empty();
 	$questionDiv.html('<h2>' + trivia[currentQuestion].question + '</h2>');
 	for(var i = 0; i < 4; i++){
-		var $answerBtn = $('<button>');
-		$answerBtn.text(trivia[currentQuestion].possibleAnswers[i]);
-		$answerBtn.attr({'data-index': i });
-		$answerBtn.addClass('answerChoice');
-		$answerBtnsDiv.append($answerBtn);
-	}
-	countDown();
-	$('.answerChoice').on('click',function(){
+		var $answerChoice = $('<li>');
+		$answerChoice.text(trivia[currentQuestion].possibleAnswers[i]);
+		$answerChoice.attr({'data-index': i });
+		$answerChoice.addClass('answer-choice');
+		$answerChoicesDiv.append($answerChoice);
+	};
+	timer();
+
+	$('.answer-choice').on('click',function(){
 		userChoice = $(this).data('index');
-		console.log(userChoice);
-		stopTimer();
+		clearInterval(time);
 		evaluate();
 	});
 }
 
 function evaluate() {
-	if (userChoice === trivia[currentQuestion].correctAnswer) {
-		alert ('yay');
+	var answerIndex = trivia[currentQuestion].correctAnswer;
+	
+	if (seconds < 1){
+		$answerChoicesDiv.empty();
+		$questionDiv.html("<p>Sorry, time's up! These are the correct ingredients: </p>"+"<p>"+trivia[currentQuestion].possibleAnswers[answerIndex]+"</p>");
+		unansweredCt++;
+
+	
+	}else if (userChoice === answerIndex) {
+		$answerChoicesDiv.empty();
+		$questionDiv.html("<p>Correct!</p>");
+		$questionDiv.append("<img src=" + trivia[currentQuestion].image + ">");
+		correctAnswerCt++;
+
 	}else{
-		alert ('nope');
-	}
+		$answerChoicesDiv.empty();
+		$questionDiv.html("<p>Nope, these are the correct ingredients: </p>"+"<p>"+trivia[currentQuestion].possibleAnswers[answerIndex]+"</p>");
+		wrongAnswerCt++;
+	};
+
+	if(currentQuestion == (trivia.length-1)){
+		setTimeout(gameOver, 4500);
+	} else{
+		currentQuestion++;
+		setTimeout(renderQuestion, 4500);
+	};	
 }
 
+function gameOver() {
+	$answerChoicesDiv.empty();
+	$startResetDiv.empty();
+	$questionDiv.empty();
 
+	$endMessageDiv.text("All done! Here's how you did:");
+	$correctCtDiv.text("Correct Answers: " + correctAnswerCt);
+	$wrongCtDiv.text("Wrong Answers: " + wrongAnswerCt);
+	$unansweredCtDiv.text("Unanswered Questions: " + unansweredCt);
 
-//QUESTION TIMER FUNCTIONS
+	var $resetButton = $('<button>');
+	$resetButton.text("Start Over")
+	$startResetDiv.append($resetButton);
 
-function thirtySecTimer() {
-	intervalId = setInterval(countDown, 1000);
+	$resetButton.on('click', function() {
+		currentQuestion = 0;
+		correctAnswerCt = 0;
+		wrongAnswerCt = 0;
+		unansweredCt = 0;
+		$endMessageDiv.empty();
+		$correctCtDiv.empty();
+		$wrongCtDiv.empty();
+		$unansweredCtDiv.empty();
+		renderQuestion();
+	});
+};
+
+//TIMER FUNCTIONS
+function timer() {
+	seconds=20;
+	$timerDiv.html("<h3>Time left: " + seconds + "</h3>");
+	time = setInterval(countDown, 1000);
 };
     
 function countDown() {
-	time--;
-	$timerDiv.html("<h3>Time left: " + time + "</h2>");
-	if (time === 0) {
-    	stop();
-		alert("Time Up!");
+	seconds--;
+	$timerDiv.html("<h3>Time left: " + seconds + "</h3>");
+	if (seconds < 1) {
+    	clearInterval(time);
+    	evaluate();
 	};
 };
 
-function stopTimer() {
-	clearInterval(intervalId);
-};
-
-thirtySecTimer();
-
-//END QUESTION TIMER FUNCTIONS
 
